f6b45626131c263fb9f2a701c24720a8e369c237
Matthew Saari <saari.matthew@gmail.com>
2022-10-04 22:22:15 -0500

    add file that does nothing

---
+++ ViewModel/AutoEquipWidget.cs
@@ -0,0 +1,18 @@
+﻿using TaleWorlds.GauntletUI;
+using TaleWorlds.GauntletUI.BaseTypes;
+
+namespace AutoEquipCompanions.ViewModel
+{
+    public class AutoEquipWidget : ButtonWidget
+    {
+        public AutoEquipWidget(UIContext context) : base(context)
+        {
+            WidthSizePolicy = SizePolicy.Fixed;
+            HeightSizePolicy = SizePolicy.Fixed;
+            SuggestedWidth = 30;
+            SuggestedHeight = 30;
+            HorizontalAlignment = HorizontalAlignment.Left;
+            VerticalAlignment = VerticalAlignment.Top;
+        }
+    }
+}
